7ab5b28251569f68b3f5984abc7be3ef75f6134c
KevenAbraham <abraham.keven20@gmail.com>
2023-11-03 01:23:30 -0300

    adicionando imagens para os perfis que o usuario escolher

--- src/pages/cadastro/Cadastro.js
+++ src/pages/cadastro/Cadastro.js
@@ -1,8 +1,21 @@
 import './Cadastro.css';
 import logo from '../../img/netflixologo.svg';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 export const Cadastro = () => {
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors },
+    } = useForm();
+
+    console.log({ errors });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className='signUpContainer'>
             <header>
@@ -13,11 +26,27 @@ export const Cadastro = () => {
                 <div className='card'>
                     <h1>Cadastrar</h1>
                     
-                    <input type='text' className='input' placeholder='Insira o seu nome' />
-                    <input type='text' className='input' placeholder='Insira o seu e-mail' />
-                    <input type='password' className='input' placeholder='Insira a sua senha' />
+                    <div className='inputsArea'>
+                        <input 
+                            type='text' 
+                            className='input' 
+                            placeholder='Insira o seu nome' 
+                        />
+                        <input 
+                            type='text' 
+                            className='input' 
+                            placeholder='Insira o seu e-mail' 
+                        />
+                        <input 
+                            type='password' 
+                            className='input' 
+                            placeholder='Insira a sua senha' 
+                        />
+                    </div>
 
-                    <Link className='link' to='/profile'><button>Cadastrar</button></Link>
+                    {/* <Link className='link' to='/profile'> */}
+                        <button>Cadastrar</button>
+                    {/* </Link> */}
                 </div>
             </form>
         </div>

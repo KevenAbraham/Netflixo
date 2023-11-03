4a3cb18429680779cd9f96e8d005f905eb455b07
KevenAbraham <abraham.keven20@gmail.com>
2023-11-03 01:41:55 -0300

    removendo as autenticações de login nos inputs

--- src/pages/cadastro/Cadastro.js
+++ src/pages/cadastro/Cadastro.js
@@ -1,21 +1,8 @@
 import './Cadastro.css';
 import logo from '../../img/netflixologo.svg';
 import { Link } from 'react-router-dom';
-import { useForm } from "react-hook-form";
 
 export const Cadastro = () => {
-    const { 
-        register, 
-        handleSubmit, 
-        formState: { errors },
-    } = useForm();
-
-    console.log({ errors });
-
-    const onSubmit = (data) => {
-        console.log(data);
-    }
-
     return (
         <div className='signUpContainer'>
             <header>
@@ -29,24 +16,21 @@ export const Cadastro = () => {
                     <div className='inputsArea'>
                         <input 
                             type='text' 
-                            className='input' 
                             placeholder='Insira o seu nome' 
                         />
                         <input 
-                            type='text' 
-                            className='input' 
+                            type='email' 
                             placeholder='Insira o seu e-mail' 
                         />
                         <input 
                             type='password' 
-                            className='input' 
                             placeholder='Insira a sua senha' 
                         />
                     </div>
 
-                    {/* <Link className='link' to='/profile'> */}
+                    <Link className='link' to='/profile'>
                         <button>Cadastrar</button>
-                    {/* </Link> */}
+                    </Link>
                 </div>
             </form>
         </div>

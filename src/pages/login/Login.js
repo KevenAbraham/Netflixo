188c9facad461e6844bbeca54e16592d0866f705
KevenAbraham <abraham.keven20@gmail.com>
2023-11-03 01:17:56 -0300

    validação de login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,21 @@
 import './Login.css';
 import logo from '../../img/netflixologo.svg';
 import { Link } from 'react-router-dom';
+import { useForm } from "react-hook-form";
 
 export const Login = () => {
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors },
+    } = useForm();
+
+    console.log({ errors });
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <div className='loginContainer'>
             <header>
@@ -13,11 +26,33 @@ export const Login = () => {
                 <h1>Entrar</h1>
 
                 <div className='inputsArea'>
-                    <input type='text' className='input' placeholder='Insira o seu e-mail' />
-                    <input type='password' className='input' placeholder='Insira a sua senha' />
-                </div>                
+                    <input
+                        className={errors?.email && "input-error"}
+                        type='email'
+                        placeholder='Insira o seu e-mail'
+                        {...register('email', { required: true })}
+                    />
+                    {errors?.email?.type == 'required' && 
+                        <p className="error-message">
+                            {errors?.email}E-mail é obrigatório.
+                        </p>
+                    }
+                    <input 
+                        className={errors?.password && "input-error"}
+                        type='password' 
+                        placeholder='Insira a sua senha'
+                        {...register('password', { required: true, minLength: 7 })}
+                    />
+                    {errors?.password?.type == 'minLength' && 
+                        <p className="error-message">
+                            {errors?.email}A senha deve ter, pelo menos, 7 caracteres.
+                        </p>
+                    }
+                </div>
 
-                <Link className='link' to='/profile'><button>Entrar</button></Link>
+                {/* <Link className='link' to='/profile'> */}
+                    <button onClick={() => handleSubmit(onSubmit)}>Entrar</button>
+                {/* </Link> */}
 
                 <div className='aboveDButton'>
                     <div className='checkbox'>

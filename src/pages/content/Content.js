d71d9bd59a635500cc1cfec595f767c1c0fd6d8b
Keven Abraham de Souza <119967485+KevenAbraham@users.noreply.github.com>
2023-11-20 01:19:53 +0000

    button in content was wrong

--- src/pages/content/Content.js
+++ src/pages/content/Content.js
@@ -25,32 +25,32 @@ export const Content = () => {
                 <div className='videoCards'>
                     <div className='thumb vid1'></div>
                     <p>Calma Madame</p>
-                    <a href='https://www.youtube.com/watch?v=wh-NyCsIUBc&t=1s'><button>Assitir</button></a>
+                    <a href='https://www.youtube.com/watch?v=wh-NyCsIUBc&t=1s'><button>Assistir</button></a>
                 </div>
                 <div className='videoCards'>
                     <div className='thumb vid2'></div>
                     <p>Calma ai Lisbelo</p>
-                    <a href='https://www.youtube.com/watch?v=kZwH4eXjx3I'><button>Assitir</button></a>
+                    <a href='https://www.youtube.com/watch?v=kZwH4eXjx3I'><button>Assistir</button></a>
                 </div>
                 <div className='videoCards'>
                     <div className='thumb vid3'></div>
                     <p>Calma ai Lamparino</p>
-                    <a href='https://www.youtube.com/watch?v=T6aKnvQnWfY'><button>Assitir</button></a>
+                    <a href='https://www.youtube.com/watch?v=T6aKnvQnWfY'><button>Assistir</button></a>
                 </div>
                 <div className='videoCards'>
                     <div className='thumb vid4'></div>
                     <p>Relaxa Mosquito</p>
-                    <a href='https://www.youtube.com/watch?v=AwVdngHssto'><button>Assitir</button></a>
+                    <a href='https://www.youtube.com/watch?v=AwVdngHssto'><button>Assistir</button></a>
                 </div>
                 <div className='videoCards'>
                     <div className='thumb vid5'></div>
                     <p>Olha o bronze do pai</p>
-                    <a href='https://www.youtube.com/watch?v=B2T6XDrDaaQ'><button>Assitir</button></a>
+                    <a href='https://www.youtube.com/watch?v=B2T6XDrDaaQ'><button>Assistir</button></a>
                 </div>
                 <div className='videoCards'>
                     <div className='thumb vid6'></div>
                     <p>Ensinando Dorival</p>
-                    <a href='https://www.youtube.com/watch?v=PQFLHzuag1E'><button>Assitir</button></a>
+                    <a href='https://www.youtube.com/watch?v=PQFLHzuag1E'><button>Assistir</button></a>
                 </div>
             </div>
         </div>
